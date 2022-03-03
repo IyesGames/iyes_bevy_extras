5c5b2fa8876f31a6c62d2c882037f5679ccdd44d
Ida Iyes <identry@protonmail.com>
2022-03-03 06:28:53 +0200

    add helpers for removing components from entities

--- src/lib.rs
+++ src/lib.rs
@@ -41,3 +41,23 @@ pub fn remove_resource<T: Send + Sync + 'static>(
 ) {
     cmd.remove_resource::<T>();
 }
+
+/// Remove a component type from all entities that have it
+pub fn remove_from_all<T: Component>(
+    mut cmd: Commands,
+    q: Query<Entity, With<T>>,
+) {
+    for e in q.iter() {
+        cmd.entity(e).remove::<T>();
+    }
+}
+
+/// Remove a component type from any entities with some other component
+pub fn remove_from_all_with<T: Component, W: Component>(
+    mut cmd: Commands,
+    q: Query<Entity, (With<T>, With<W>)>,
+) {
+    for e in q.iter() {
+        cmd.entity(e).remove::<T>();
+    }
+}
